8bbb1e1aee848e3b3b037f9d7b51812f01c693dd
Maxwell Nderitu <nderitumaxwell@gmail.com>
2021-06-11 18:45:15 +0300

    Fix: Memory leak issue

--- libs/scraper/providers/eztv/src/eztv-provider.service.ts
+++ libs/scraper/providers/eztv/src/eztv-provider.service.ts
@@ -1,75 +1,75 @@
-import { Inject, Injectable, Logger } from '@nestjs/common'
-import { BaseProvider, ScrapedItem, ScraperProviderConfig } from '@pct-org/scraper/providers/base'
-import { EztvApi } from '@pct-org/eztv-api'
-import * as pMap from 'p-map'
-import { ShowHelperService } from '@pct-org/scraper/helpers/show'
-import { SHOW_TYPE } from '@pct-org/types/show'
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { BaseProvider, ScrapedItem, ScraperProviderConfig } from '@pct-org/scraper/providers/base';
+import { EztvApi } from '@pct-org/eztv-api';
+import * as pLimit from 'p-limit';
+import { ShowHelperService } from '@pct-org/scraper/helpers/show';
+import { SHOW_TYPE } from '@pct-org/types/show';
 
 @Injectable()
 export class EztvProviderService extends BaseProvider {
 
   @Inject('ShowHelperService')
-  protected readonly showHelper: ShowHelperService
+  protected readonly showHelper: ShowHelperService;
 
-  name = 'EZTV'
+  name = 'EZTV';
 
-  maxWebRequests = 2
+  maxWebRequests = 10;
 
-  logger = new Logger(this.name)
+  logger = new Logger(this.name);
 
   configs: ScraperProviderConfig[] = [{
-    contentType: SHOW_TYPE
-  }]
+    contentType: SHOW_TYPE,
+  }];
 
-  api
+  api;
 
   constructor() {
-    super()
+    super();
 
-    this.api = new EztvApi()
+    this.api = new EztvApi();
   }
 
   protected async scrapeConfig(config: ScraperProviderConfig): Promise<void> {
-    this.setConfig(config)
+    this.setConfig(config);
 
-    const contents: ScrapedItem[] = await this.api.getAllShows()
+    const contents: ScrapedItem[] = await this.api.getAllShows();
 
-    this.logger.log(`${this.name}: Found ${contents.length} ${this.contentType}s.`)
+    this.logger.log(`${this.name}: Found ${contents.length} ${this.contentType}s.`);
 
-    await pMap(
-      contents,
-      async (content) => {
-        const isInBlacklist = await this.isItemBlackListed(content)
+    const limit = pLimit(this.maxWebRequests);
 
-        // Only get data for this item if it's not in the blacklist
-        if (!isInBlacklist) {
-          try {
-            // Get full show data
-            const show: ScrapedItem = await this.api.getShowData(content)
+    const handleShow = async (content) => {
+      const isInBlacklist = await this.isItemBlackListed(content);
 
-            // Enhance and import the show
-            await this.enhanceAndImport(show)
+      // Only get data for this item if it's not in the blacklist
+      if (!isInBlacklist) {
+        try {
+          // Get full show data
+          const show: ScrapedItem = await this.api.getShowData(content);
 
-          } catch (err) {
-            const errorMessage = err.message || err
+          // Enhance and import the show
+          await this.enhanceAndImport(show);
 
-            this.logger.error(`EztvProviderService.scrapeConfig: ${errorMessage}`, err.stack)
+        } catch (err) {
+          const errorMessage = err.message || err;
 
-            // Log the content so it can be better debugged from logs
-            if (errorMessage.includes('Could not find any data with slug')) {
-              this.logger.error(JSON.stringify(content))
-            }
+          this.logger.error(`EztvProviderService.scrapeConfig: ${errorMessage}`, err.stack);
+
+          // Log the content so it can be better debugged from logs
+          if (errorMessage.includes('Could not find any data with slug')) {
+            this.logger.error(JSON.stringify(content));
           }
         }
-      },
-      {
-        concurrency: this.maxWebRequests
       }
-    )
+    };
+
+    await Promise.all(contents.map(content => {
+      return limit(() => handleShow(content));
+    }));
   }
 
   extractContent({ torrent, regex, lang }: { torrent: any; regex: any; lang: any }): undefined {
-    return undefined
+    return undefined;
   }
 
 }
